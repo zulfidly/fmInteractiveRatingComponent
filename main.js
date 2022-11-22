22ad614c1959db2ce6d12df18f8ecbfaae49262f
zulfidly <114971952+zulfidly@users.noreply.github.com>
2022-11-22 13:19:06 +0800

    test1

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const star3 = document.getElementById("star3");
 const star4 = document.getElementById("star4");
 const star5 = document.getElementById("star5");
 const submit = document.getElementById("submitBtn");
+const numOfStars = document.getElementById("numOfStars");
 
 submit.addEventListener("click", () => {
     document.getElementById("ratingULid").style.display = "none";
     document.getElementById("thkYouULid").style.display = "flex";
-    document.getElementById("numOfStars").innerHTML = value; 
+    // numOfStars.innerHTML = value; 
 })
 star1.addEventListener("click", () => {
     star1.classList.toggle("active");
@@ -17,7 +18,7 @@ star1.addEventListener("click", () => {
     star4.classList.remove("active");
     star5.classList.remove("active");
     // console.log(document.getElementById("star1").getAttribute("value"));
-    document.getElementById("numOfStars").innerHTML = document.getElementById("star1").getAttribute("value");
+    numOfStars.innerHTML = star1.getAttribute("value");
 })
 star2.addEventListener("click", () => {
     star1.classList.remove("active");
@@ -25,7 +26,7 @@ star2.addEventListener("click", () => {
     star3.classList.remove("active");
     star4.classList.remove("active");
     star5.classList.remove("active");
-    document.getElementById("numOfStars").innerHTML = document.getElementById("star2").getAttribute("value");
+    numOfStars.childNodes[0].nodeValue = document.getElementById("star2").getAttribute("value");
 })
 star3.addEventListener("click", () => {
     star1.classList.remove("active");
@@ -33,7 +34,7 @@ star3.addEventListener("click", () => {
     star3.classList.toggle("active");
     star4.classList.remove("active");
     star5.classList.remove("active");
-    document.getElementById("numOfStars").innerHTML = document.getElementById("star3").getAttribute("value");
+    document.getElementById("numOfStars").innerText = document.getElementById("star3").getAttribute("value");
 })
 star4.addEventListener("click", () => {
     star1.classList.remove("active");
@@ -41,7 +42,7 @@ star4.addEventListener("click", () => {
     star3.classList.remove("active");
     star4.classList.toggle("active");
     star5.classList.remove("active");
-    document.getElementById("numOfStars").innerHTML = document.getElementById("star4").getAttribute("value");
+    document.getElementById("numOfStars").textContent = document.getElementById("star4").getAttribute("value");
 })
 star5.addEventListener("click", () => {
     star1.classList.remove("active");
